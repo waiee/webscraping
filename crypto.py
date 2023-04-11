4e450f39ea028623f5d9cc557ff55bf5ef700624
Waiee Zainol <69743222+waiee@users.noreply.github.com>
2023-04-11 11:05:58 +0800

    get crypto prices

--- crypto.py
+++ crypto.py
@@ -18,4 +18,8 @@ prices = {}
 for tr in trs[:10]:
     name, price = tr.contents[2:4]
     fixed_name = name.p.string
-    print(price)
+    fixed_price = price.a.string
+
+    prices[fixed_name] = fixed_price
+
+print(prices)
