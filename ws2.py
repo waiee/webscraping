23c618fa91bd3eba4ff9139912f26f9973f9690c
Waiee Zainol <69743222+waiee@users.noreply.github.com>
2023-04-11 08:11:08 +0800

    find specific tags

--- ws2.py
+++ ws2.py
@@ -4,8 +4,11 @@ import requests
 with open("index.html", "r") as f:
     doc = BeautifulSoup(f, "html.parser")
 
-tag = doc.find("option")
-tag['value'] = 'false'
-tag['color'] = "blue"
+# tag = doc.find("option")
+# tag['value'] = 'false'
+# tag['color'] = "blue"
 # print(tag.attrs) #print in dict
+
+# tag = doc.find_all(["p", "div", "li"]) #search multiple tags
+tag = doc.find_all(["option"], text="Undergraduate", value="undergraduate")
 print(tag)
