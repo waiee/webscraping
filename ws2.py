b29e20feb24ee2c29b1c52d5f20c59953e22dd43
Waiee Zainol <69743222+waiee@users.noreply.github.com>
2023-04-11 08:12:55 +0800

    import re

--- ws2.py
+++ ws2.py
@@ -1,5 +1,6 @@
 from bs4 import BeautifulSoup
 import requests
+import re
 
 with open("index.html", "r") as f:
     doc = BeautifulSoup(f, "html.parser")
@@ -10,5 +11,6 @@ with open("index.html", "r") as f:
 # print(tag.attrs) #print in dict
 
 # tag = doc.find_all(["p", "div", "li"]) #search multiple tags
-tag = doc.find_all(["option"], text="Undergraduate", value="undergraduate")
+# tag = doc.find_all(["option"], text="Undergraduate", value="undergraduate")
+tag = doc.find_all(class_="btn-item")
 print(tag)
