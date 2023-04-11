2ed5094ffb6de13c9601d1bdb20fb3ef742271f8
Waiee Zainol <69743222+waiee@users.noreply.github.com>
2023-04-11 08:29:06 +0800

    siblings tbrow

--- crypto.py
+++ crypto.py
@@ -5,5 +5,8 @@ url = "https://coinmarketcap.com"
 result = requests.get(url).text
 doc = BeautifulSoup(result, "html.parser")
 
-tbody = doc.tbody
-print(tbody)
+tbody = doc.tbody #read whole body
+trs = tbody.contents #table row
+
+print(list(trs[0].next_siblings)) #next sibling
+# print(trs[1].previous_sibling) #prev sibling
