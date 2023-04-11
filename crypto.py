e3026f9e8cd2ac7082e8cf7f4c159ddcf8d0071d
Waiee Zainol <69743222+waiee@users.noreply.github.com>
2023-04-11 10:57:49 +0800

    ws

--- crypto.py
+++ crypto.py
@@ -8,5 +8,11 @@ doc = BeautifulSoup(result, "html.parser")
 tbody = doc.tbody #read whole body
 trs = tbody.contents #table row
 
-print(list(trs[0].next_siblings)) #next sibling
 # print(trs[1].previous_sibling) #prev sibling
+# print(list(trs[0].next_siblings)) #next sibling
+
+# print(trs[0].parent.name) #parent name
+# print(list(trs[0].descendants)) #everything inside tag
+
+prices = {}
+for tr in trs
